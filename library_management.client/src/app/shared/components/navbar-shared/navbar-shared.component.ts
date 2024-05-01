a93f2bac25f05eb051932cb8965009708fc86af7
gargisawaria <gargisawaria1234@gmail.com>
2024-05-01 13:59:10 +0530

    navbar-toggle-button

--- library_management.client/src/app/shared/components/navbar-shared/navbar-shared.component.ts
+++ library_management.client/src/app/shared/components/navbar-shared/navbar-shared.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-shared',
@@ -6,5 +7,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar-shared.component.css']
 })
 export class NavbarSharedComponent {
+  activeButton: string ='';
+  isTogglerClicked: boolean = false;
+  isDropdownOpen: boolean = false;
+  getrole: any;
+  role: any;
+  constructor(private router: Router) {
+
+  }
+  ngOnInit(): void
+  {
+    this.getRole();
+    if (this.role=='User')
+      this.toggleActiveButton('readers-hub');
+    if (this.role == 'Admin')
+   this.toggleActiveButton('Dashboard')
+  }
+
+  toggleActiveButton(button: string) {
+    this.activeButton = button;
+
+    if (button === 'my-books') {
+      this.router.navigate(['/my-books']);
+    }
+
+    else if(button === 'readers-hub') {
+      this.router.navigate(['/']);
+    }
+    else if (button === 'Dashboard') {
+      this.router.navigate(['/admin']);
+    }
+    else if (button === 'manage-books') {
+      this.router.navigate(['/']);
+    }
+    this.isTogglerClicked = false;
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+  getRole() {
+    this.getrole = localStorage.getItem('user');
+    
+    const user = JSON.parse(this.getrole);
+
+    this.role = user.role;
+
+  }
 
 }
