2531a201e3a46ff55c5b088107cd2b02f3a80942
AzureAD\VanditJain <vandit.jain@cginfinity.com>
2024-05-03 10:58:42 +0530

    mai hu kaun don bhai bunty ka

---
+++ LIBRARY_MANAGEMENT.Server/Controllers/RoleController.cs
@@ -0,0 +1,17 @@
+﻿using LIBRARY_MANAGEMENT.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LIBRARY_MANAGEMENT.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ActionController:ControllerBase
+    {
+        private readonly LibraryManagementSystemContext _context;
+
+        public ActionController(LibraryManagementSystemContext context)
+        {
+            _context = context;
+        }
+    }
+}
