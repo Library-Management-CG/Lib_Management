eaf469b550130efe19dd9bb75aea3847e4012928
gargisawaria <gargisawaria1234@gmail.com>
2024-05-07 17:19:32 +0530

    12345

--- library_management.client/src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ library_management.client/src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 declare var $: any;
-counti: number[] = [];
+
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
   styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent {
+  counti: number[] = [];
   ngOnInit(): void {
-    $(document).ready(function () {
-      $('#exampleModalCenter').modal('show');
-    });
+    //$(document).ready(function () {
+    //  $('#exampleModalCenter').modal('show');
+    //});
       for (let i = 1; i <= 10; i++) {
           this.counti.push(i);
       }
