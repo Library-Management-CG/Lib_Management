bfc20a87c5f9e406deb8e322dcc715052be5bc72
gargisawaria <gargisawaria1234@gmail.com>
2024-05-07 17:32:14 +0530

    last-commit-eod

--- library_management.client/src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ library_management.client/src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -16,7 +16,60 @@ export class AdminDashboardComponent {
           this.counti.push(i);
       }
   }
-  
+  mostPopularBooks = [
+
+    {
+      title: 'The Invisible Cloud',
+      author: 'Daryl Bishop & Nick Smith',
+      imageUrl: '../../../assets/icons/Book - The Invisible Cloud.svg',
+      ratingUrl: 4,
+      numberOfPeopleReviewed: 28
+    },
+
+    {
+      title: 'The Invisible',
+      author: 'Daryl Bishop & Nick Smith',
+      imageUrl: '../../../assets/icons/Book - The Invisible Cloud.svg',
+      ratingUrl: 3,
+      numberOfPeopleReviewed: 28
+    },
+
+
+
+    {
+      title: 'The Cloud',
+      author: 'Daryl Bishop & Nick Smith',
+      imageUrl: '../../../assets/icons/Book - The Invisible Cloud.svg',
+      ratingUrl: 1,
+      numberOfPeopleReviewed: 28
+    },
+
+    {
+      title: 'The ok',
+      author: 'Daryl Bishop & Nick Smith',
+      imageUrl: '../../../assets/icons/Book - The Invisible Cloud.svg',
+      ratingUrl: 2,
+      numberOfPeopleReviewed: 28
+    },
+
+    {
+      title: 'my',
+      author: 'Daryl Bishop & Nick Smith',
+      imageUrl: '../../../assets/icons/Book - The Invisible Cloud.svg',
+      ratingUrl: 3,
+      numberOfPeopleReviewed: 28
+    },
+
+    {
+      title: 'daryl',
+      author: 'Daryl Bishop & Nick Smith',
+      imageUrl: '../../../assets/icons/Book - The Invisible Cloud.svg',
+      ratingUrl: 5,
+      numberOfPeopleReviewed: 28
+    },
+
+
+  ];
 
  
 
