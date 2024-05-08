962c3a01c5e69191ef54e9c6098cd482f2f300c5
AzureAD\VanditJain <vandit.jain@cginfinity.com>
2024-05-08 10:54:46 +0530

    commit routing change

--- library_management.client/src/app/app-routing.module.ts
+++ library_management.client/src/app/app-routing.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule,Routes } from '@angular/router';
-import { AccessControlWebComponent } from './admin/access-control-web/access-control-web.component';
-import { AccessControlMobileComponent } from './admin/access-control-mobile/access-control-mobile.component';
+
 
 const routes: Routes = [
 
  
 
 
-  { path: 'admin/accesscontrol', component: AccessControlWebComponent },
-  { path: 'admin/accesscontrolmobile', component: AccessControlMobileComponent },
+ 
 
 ];
 
