2115c384119cd5461f1010183f6cfcde5b09835a
Harleen Chadha <harleen.chadha@cginfinity.com>
2024-05-13 17:30:30 +0530

    changes

--- library_management.client/src/app/user/explore-books/explore-books.component.ts
+++ library_management.client/src/app/user/explore-books/explore-books.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./explore-books.component.css']
 })
 export class ExploreBooksComponent {
+  isChecked: boolean = false;
 
+  toggleCheckbox(): void {
+    this.isChecked = !this.isChecked;
+  }
+  c: any = 0;
+  c1: any = 0;
+  c2: any = 0;
+  c3: any = 0;
+  c4: any = 0;
+
+  handleCheckboxChange() { }
 }
