94340b4038b3ae3f4c9fb8d405cbc2cbe4d357ea
Harleen Chadha <harleen.chadha@cginfinity.com>
2024-05-21 17:19:06 +0530

    commit

--- library_management.client/src/app/user/new-explore-books/new-explore-books.component.ts
+++ library_management.client/src/app/user/new-explore-books/new-explore-books.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-explore-books',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-explore-books.component.css']
 })
 export class NewExploreBooksComponent {
+  constructor(private router: Router) { };
+  isChecked: boolean = false;
 
+  toggleCheckbox(): void {
+    this.isChecked = !this.isChecked;
+  }
+  redirect_back() {
+    this.redirectToUserDashboard();
+  }
+  redirectToUserDashboard() {
+    this.router.navigate(['']);
+  }
 }
