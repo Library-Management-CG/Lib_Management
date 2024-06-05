da4313cd509f0110f1ea5796efdee76cbd90dfeb
divyanshrampal28 <divyansh.rampal@cginfinity.com>
2024-06-05 16:33:11 +0530

    commit

--- library_management.client/src/app/user/user-routing.module.ts
+++ library_management.client/src/app/user/user-routing.module.ts
@@ -5,7 +5,6 @@ import { UserComponent } from './user.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MyBooksComponent } from './my-books/my-books.component';
 import { MyBooksMobileComponent } from './my-books-mobile/my-books-mobile.component';
-import { ExploreBooksComponent } from './explore-books/explore-books.component';
 import { NewExploreBooksComponent } from './new-explore-books/new-explore-books.component';
 
 const routes: Routes = [
@@ -22,11 +21,6 @@ const routes: Routes = [
   {
     path: 'my-books-mobile',
     component: MyBooksMobileComponent,
-
-  }
-  , {
-    path: 'explore-books',
-    component: ExploreBooksComponent,
 
   }
   , {
