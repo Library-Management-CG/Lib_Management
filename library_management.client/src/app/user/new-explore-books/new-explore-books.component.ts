c9a3560739ff038559f6112b5c55be35c7357a2c
Harleen Chadha <harleen.chadha@cginfinity.com>
2024-06-19 16:54:39 +0530

    naman all testing done

--- library_management.client/src/app/user/new-explore-books/new-explore-books.component.ts
+++ library_management.client/src/app/user/new-explore-books/new-explore-books.component.ts
@@ -60,12 +60,15 @@ export class NewExploreBooksComponent {
       //}, 1000)
       //setTimeout(() => {
       //  this.infinite = false;
-      //},2000)
+      //},000)
       
       this.pageNumber++;
       this.loading = true;
       //console.log(this.pageNumber);
-       this.exploreBookData();
+      setTimeout(() => {
+        this.exploreBookData();
+      },500)
+       
     }
   }
 
@@ -76,7 +79,7 @@ export class NewExploreBooksComponent {
     //if (this.infinite) {
     //  return scrollPosition + windowHeight >= documentHeight+200;
     //}
-    return scrollPosition + windowHeight >= documentHeight;
+    return scrollPosition + windowHeight >= documentHeight-20;
   }
 
   exploreBookData() {
